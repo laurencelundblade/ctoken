3a340d4c3ebac49ae1845eafc6f08aedff41486e
Laurence Lundblade <lgl@island-resort.com>
2020-02-19 15:07:14 +0100

    Add documentation and prototypes for CWT encoding/decoding

--- inc/ctoken_cwt_decode.h
+++ inc/ctoken_cwt_decode.h
@@ -24,7 +24,211 @@ extern "C" {
 #endif
 #endif
 
-// TODO: Make prototypes of these and document them
+
+
+/**
+ * \brief Decode the issuer.
+ *
+ * \param[in] context  The decoding context to decode from.
+ * \param[out] issuer  Place to put pointer and length of the issuer claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not a text string.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not found in token.
+ *
+ *  The principle that created the token. It is a text string or a URI.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_issuer(struct ctoken_decode_ctx *context,
+                         struct q_useful_buf_c *issuer);
+
+
+/**
+ * \brief Decode the subject claim.
+ *
+ * \param[in] context   The decoding context to decode from.
+ * \param[out] subject  Place to put pointer and length of the subject claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not a text string.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not found in token.
+ *
+ * Identifies the subject of the token. It is a text string or URI.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_subject(struct ctoken_decode_ctx *context,
+                          struct q_useful_buf_c *subject);
+
+
+/**
+ * \brief Decode the audience claim.
+ *
+ * \param[in] context    The decoding context to decode from.
+ * \param[out] audience  Place to put pointer and length of the audience claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not a text string.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not found in token.
+ *
+ * This identifies the recipient for which the token is intended. It is
+ * a text string or URI.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_audience(struct ctoken_decode_ctx *context,
+                           struct q_useful_buf_c *audience);
+
+
+/**
+ * \brief Decode the expiration claim.
+ *
+ * \param[in] context      The decoding context to decode from.
+ * \param[out] expiration  Place to return expiration claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not an integer.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not found in token.
+ *
+ * The time format is that described as Epoch Time in CBOR, RFC 7049, the
+ * number of seconds since Jan 1, 1970.
+ *
+ * This implementation only supports int64_t time, not floating point,
+ * even though the specification allows floating point.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_expiration(struct ctoken_decode_ctx *context,
+                             int64_t *expiration);
+
+
+/**
+ * \brief Decode the not-before claim.
+ *
+ * \param[in] context      The decoding context to decode from.
+ * \param[out] not_before  Place to return the not-before claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not an integer.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not found in token.
+ *
+ * The time format is that described as Epoch Time in CBOR, RFC 7049, the
+ * number of seconds since Jan 1, 1970.
+ *
+ * This implementation only supports int64_t time, not floating point,
+ * even though the specification allows floating point.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_not_before(struct ctoken_decode_ctx *context,
+                             int64_t *not_before);
+
+
+/**
+ * \brief Decode the issued-at claim.
+ *
+ * \param[in] context  The decoding context to decode from.
+ * \param[out] iat     Place to put pointer and length of the issued-at claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not a byte string.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not an integer.
+ *
+ * The time at which the token was issued at.
+ *
+ * The time format is that described as Epoch Time in CBOR, RFC 7049, the
+ * number of seconds since Jan 1, 1970.
+ *
+ * This implementation only supports int64_t time, not floating point,
+ * even though the specification allows floating point.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_iat(struct ctoken_decode_ctx *context,
+                      int64_t *iat);
+
+
+/**
+ * \brief Decode the CWT ID claim.
+ *
+ * \param[in] context  The decoding context to decode from.
+ * \param[out] cti     Place to put pointer and length of the CWT ID claim.
+ *
+ * \retval CTOKEN_ERR_CBOR_STRUCTURE
+ *         General structure of the token is incorrect, for example
+ *         the top level is not a map or some map wasn't closed.
+ *
+ * \retval CTOKEN_ERR_CBOR_NOT_WELL_FORMED
+ *         CBOR syntax is wrong and it is not decodable.
+ *
+ * \retval CTOKEN_ERR_CBOR_TYPE
+ *         Returned if the claim is not a byte string.
+ *
+ * \retval CTOKEN_ERR_NOT_FOUND
+ *         Data item for \c label was not found in token.
+ *
+ * This is a byte string that uniquely identifies the token.
+ */
+static inline enum ctoken_err_t
+ctoken_decode_cwt_cti(struct ctoken_decode_ctx *context,
+                      struct q_useful_buf_c *cti);
+
+
+
+
+/* --------------------------------------------------------------------------
+ *       Inline implementations
+ * --------------------------------------------------------------------------*/
+
 
 static inline enum ctoken_err_t
 ctoken_decode_cwt_issuer(struct ctoken_decode_ctx *me,
--- inc/ctoken_cwt_encode.h
+++ inc/ctoken_cwt_encode.h
@@ -17,8 +17,145 @@
 #include "ctoken_cwt_labels.h"
 
 
+#ifdef __cplusplus
+extern "C" {
+#ifdef 0
+} /* Keep editor indention formatting happy */
+#endif
+#endif
 
-// TODO: Make prototypes of these and document them
+
+/**
+ * \brief Encode the issuer in to the token.
+ *
+ * \param[in] context   The token encoder context.
+ * \param[in] issuer    Pointer and length of issuer.
+ *
+ * The principle that created the token. It is a text string or a URI.
+
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_issuer(struct ctoken_encode_ctx *context,
+                                            struct q_useful_buf_c issuer);
+
+
+/**
+ * \brief Encode the subject in to the token.
+ *
+ * \param[in] context  The token encoder context.
+ * \param[in] subject  Pointer and length of subject.
+ *
+ * Identifies the subject of the token. It is a text string or URI.
+ *
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_subject(struct ctoken_encode_ctx *context,
+                                             struct q_useful_buf_c subject);
+
+
+/**
+ * \brief Encode the audience in to the token.
+ *
+ * \param[in] context   The token encoder context.
+ * \param[in] audience  Pointer and length of audience.
+ *
+ * This identifies the recipient for which the token is intended. It is
+ * a text string or URI.
+ *
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_audience(struct ctoken_encode_ctx *context,
+                                              struct q_useful_buf_c audience);
+
+
+/**
+ * \brief Encode the expiration time in to the token.
+ *
+ * \param[in] context     The token encoder context.
+ * \param[in] expiration  The expiration time to encode.
+ *
+ * The time format is that described as Epoch Time in CBOR, RFC 7049, the
+ * number of seconds since Jan 1, 1970.
+ *
+ * This implementation only supports int64_t time, not floating point,
+ * even though the specification allows floating point.
+ *
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_expiration(struct ctoken_encode_ctx *context,
+                                                int64_t expiration);
+
+
+/**
+ * \brief Encode the not-before claim in to the token.
+ *
+ * \param[in] context      The token encoder context.
+ * \param[in] not_before   The not-before time to encode.
+ *
+ * The time format is that described as Epoch Time in CBOR, RFC 7049, the
+ * number of seconds since Jan 1, 1970.
+ *
+ * This implementation only supports int64_t time, not floating point,
+ * even though the specification allows floating point.
+ *
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_not_before(struct ctoken_encode_ctx *context,
+                                                int64_t not_before);
+
+
+/**
+ * \brief Encode the "issued-at" in to the token.
+ *
+ * \param[in] context  The token encoder context.
+ * \param[in] iat      The issued-at time.
+ *
+ * The time at which the token was issued at.
+ *
+ * The time format is that described as Epoch Time in CBOR, RFC 7049, the
+ * number of seconds since Jan 1, 1970.
+ *
+ * This implementation only supports int64_t time, not floating point,
+ * even though the specification allows floating point.
+ *
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_iat(struct ctoken_encode_ctx *context,
+                                         int64_t iat);
+
+
+/**
+ * \brief Encode the CWT claim ID in to the token.
+ *
+ * \param[in] context   The token encoder context.
+ * \param[in] cti       Pointer and length of CWT claim ID.
+ *
+ * This is a byte string that uniquely identifies the token.
+ *
+ * If there is an error like insufficient space in the output buffer,
+ * the error state is entered. It is returned later when ctoken_encode_finish()
+ * is called.
+ */
+static inline void ctoken_encode_cwt_cti(struct ctoken_encode_ctx *context,
+                                         struct q_useful_buf_c cti);
+
+
+
+/* --------------------------------------------------------------------------
+ *       Inline implementations
+ * --------------------------------------------------------------------------*/
 
 
 static inline void ctoken_encode_cwt_issuer(struct ctoken_encode_ctx *me,
@@ -43,16 +180,13 @@ static inline void ctoken_encode_cwt_audience(struct ctoken_encode_ctx *me,
 static inline void ctoken_encode_cwt_expiration(struct ctoken_encode_ctx *me,
                                                 int64_t expiration)
 {
-    // TAG by time? No... TODO:
     ctoken_encode_add_integer(me, CTOKEN_CWT_LABEL_EXPIRATION, expiration);
 }
+
 
 static inline void ctoken_encode_cwt_not_before(struct ctoken_encode_ctx *me,
                                                int64_t not_before)
 {
-    // Per RFC 8239 (CWT) this can be a float. That is not handled
-    // here. TODO: figure out if the standard should allow floats
-    // or not.
     ctoken_encode_add_integer(me, CTOKEN_CWT_LABEL_NOT_BEFORE, not_before);
 }
 
