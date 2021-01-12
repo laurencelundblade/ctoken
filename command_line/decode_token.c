30139261cf5b5debdad3fa733e2fa7823606a688
Laurence Lundblade <lgl@securitytheory.com>
2021-01-11 21:06:43 -0800

    getting started with command line

---
+++ command_line/decode_token.c
@@ -0,0 +1,9 @@
+//
+//  decode_token.c
+//  CToken
+//
+//  Created by Laurence Lundblade on 1/11/21.
+//  Copyright © 2021 Laurence Lundblade. All rights reserved.
+//
+
+#include "decode_token.h"
---
+++ command_line/decode_token.h
@@ -0,0 +1,14 @@
+//
+//  decode_token.h
+//  CToken
+//
+//  Created by Laurence Lundblade on 1/11/21.
+//  Copyright © 2021 Laurence Lundblade. All rights reserved.
+//
+
+#ifndef decode_token_h
+#define decode_token_h
+
+#include <stdio.h>
+
+#endif /* decode_token_h */
