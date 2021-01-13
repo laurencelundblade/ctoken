4b7027815dd1b0dd7a77fe5a816e546d6e0f4428
Laurence Lundblade <lgl@securitytheory.com>
2021-01-12 23:07:15 -0800

    more stuff

--- command_line/decode_token.c
+++ command_line/decode_token.c
@@ -7,3 +7,53 @@
 //
 
 #include "decode_token.h"
+#include "t_cose/q_useful_buf.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Read the contents of a file into malloced buffer
+ *
+ *
+ */
+struct q_useful_buf_c read_file(int file_descriptor)
+{
+    char    input_buf[2048];
+    char   *file_content;
+    size_t  file_size;
+    ssize_t amount_read;
+
+
+    file_content = NULL;
+    file_size    = 0;
+    while(1) {
+        amount_read = read(file_descriptor, input_buf, sizeof(input_buf));
+
+        if(amount_read == 0) {
+            /* normal exit */
+            break;
+        }
+
+        if(amount_read < 0) {
+            /* read error exit */
+            return NULL_Q_USEFUL_BUF_C;
+        }
+
+        if(file_content == NULL) {
+            file_content = malloc(amount_read);
+            if(file_content == NULL) {
+                return NULL_Q_USEFUL_BUF_C;
+            }
+        } else {
+            file_content = realloc(file_content, amount_read + file_size);
+        }
+
+        memcpy(file_content + file_size, input_buf, amount_read);
+        file_size += amount_read;
+    }
+
+    return (struct q_useful_buf_c){file_content, file_size};
+}
+
+
--- command_line/decode_token.h
+++ command_line/decode_token.h
@@ -11,4 +11,67 @@
 
 #include <stdio.h>
 
+
+
+/*
+ -claim 44:xx
+
+ -in-form CBOR, JSON
+ -out_form CBOR, JSON, text, CBOR diag
+ -in <file>
+ -out <file>
+ -in_prot none, sign, mac, sign_encrypt, mac_encrypt, auto
+ -out_prot none, sign, mac, sign_encrypt, mac_encrypt
+
+ -noverify  The input file will be decoded, but any signature or mac will not be verified. No need to supply key material
+
+ -out_sign_alg
+ -out_encrypt_alg
+ -out_sign_key  private key to sign with
+ -out_encrypt_key public key to encrypt with
+ -out_certs  certs to include in the output token
+
+ -in_verify_key
+ -in_verify_cert
+ -in_decrypt_keyh
+
+ -out_tag  none, full, cose
+
+
+
+
+
+
+
+
+
+There must be an input that is either a file or some claims.
+ If there is a file, it will be verified and key material must be given to do so.
+ To skip verification use the -noverify option
+
+
+
+
+
+
+ */
+
+struct arguments {
+    const char *input_file;
+    const char *output_file;
+
+    int input_format;
+    int output_format;
+
+    int input_protection;
+    int output_protection;
+
+    char **claims;
+
+
+
+
+};
+
+
 #endif /* decode_token_h */
